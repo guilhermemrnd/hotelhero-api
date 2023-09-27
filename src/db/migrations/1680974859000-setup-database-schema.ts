3fd215205506424ac4c2244e12dbeb35694400e2
Guilherme Miranda <guilherme_de_oliveira_n1@hotmail.com>
2023-09-27 17:47:45 -0300

    chore: fix setup database schema migration

--- src/db/migrations/1680974859000-setup-database-schema.ts
+++ src/db/migrations/1680974859000-setup-database-schema.ts
@@ -6,32 +6,18 @@ export class SetupDatabaseSchema1680974859000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
         CREATE TABLE "users" (
-            "id" uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+            "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
             "name" character varying(100) NOT NULL,
             "email" character varying(75) UNIQUE NOT NULL,
             "password" character varying(255) NOT NULL,
-            "created_at" TIMESTAMP NOT NULL DEFAULT now(),
-            "updated_at" TIMESTAMP NOT NULL DEFAULT now()
-        )
-    `);
-
-    await queryRunner.query(`
-        CREATE TABLE "regions" (
-            "id" SERIAL PRIMARY KEY,
-            "name" character varying(100) NOT NULL
-        )
-    `);
-
-    await queryRunner.query(`
-        CREATE TABLE "amenities" (
-            "id" SERIAL PRIMARY KEY,
-            "name" character varying(50) UNIQUE NOT NULL
+            "created_at" TIMESTAMP DEFAULT now(),
+            "updated_at" TIMESTAMP DEFAULT now()
         )
     `);
 
     await queryRunner.query(`
         CREATE TABLE "hotels" (
-            "id" SERIAL PRIMARY KEY,
+            "id" serial PRIMARY KEY,
             "name" character varying(100) NOT NULL,
             "address" character varying(255) NOT NULL,
             "rating" float,
@@ -39,39 +25,42 @@ export class SetupDatabaseSchema1680974859000 implements MigrationInterface {
             "daily_price" float NOT NULL,
             "currency_code" character varying NOT NULL,
             "description" character varying(2048),
-            "photos" text[] NOT NULL,
+            "photos" text[],
             "max_guests" integer,
             "bathrooms" integer,
-            "regionId" integer,
-            CONSTRAINT "FK_region_hotels" FOREIGN KEY ("regionId") REFERENCES "regions"("id")
+            "region_id" integer
+        )
+    `);
+
+    await queryRunner.query(`
+        CREATE TABLE "regions" (
+            "id" serial PRIMARY KEY,
+            "name" character varying(100) NOT NULL
         )
     `);
 
     await queryRunner.query(`
         CREATE TABLE "bookings" (
-            "id" uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-            "user_id" uuid,
-            "hotel_id" integer,
-            "check_in" TIMESTAMP NOT NULL,
-            "check_out" TIMESTAMP NOT NULL,
+            "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+            "user_id" uuid NOT NULL,
+            "hotel_id" integer NOT NULL,
+            "check_in" date NOT NULL,
+            "check_out" date NOT NULL,
             "number_of_guests" integer NOT NULL,
             "total_cost" float NOT NULL,
             "is_paid" boolean DEFAULT false,
             "payment_id" character varying,
-            "created_at" TIMESTAMP NOT NULL DEFAULT now(),
-            "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
-            CONSTRAINT "FK_booking_user" FOREIGN KEY ("user_id") REFERENCES "users"("id"),
-            CONSTRAINT "FK_booking_hotel" FOREIGN KEY ("hotel_id") REFERENCES "hotels"("id")
+            "created_at" TIMESTAMP DEFAULT now(),
+            "updated_at" TIMESTAMP DEFAULT now(),
+            FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE,
+            FOREIGN KEY ("hotel_id") REFERENCES "hotels"("id") ON DELETE CASCADE
         )
     `);
 
     await queryRunner.query(`
-        CREATE TABLE "user_favorite_hotels" (
-            "user_id" uuid,
-            "hotel_id" integer,
-            CONSTRAINT "PK_user_hotel" PRIMARY KEY ("user_id", "hotel_id"),
-            CONSTRAINT "FK_user" FOREIGN KEY ("user_id") REFERENCES "users"("id"),
-            CONSTRAINT "FK_hotel" FOREIGN KEY ("hotel_id") REFERENCES "hotels"("id")
+        CREATE TABLE "amenities" (
+            "id" serial PRIMARY KEY,
+            "name" character varying(50) UNIQUE NOT NULL
         )
     `);
 
@@ -79,19 +68,34 @@ export class SetupDatabaseSchema1680974859000 implements MigrationInterface {
         CREATE TABLE "hotel_amenities" (
             "hotel_id" integer,
             "amenity_id" integer,
-            CONSTRAINT "PK_hotel_amenity" PRIMARY KEY ("hotel_id", "amenity_id"),
-            CONSTRAINT "FK_amenity_hotel" FOREIGN KEY ("hotel_id") REFERENCES "hotels"("id"),
-            CONSTRAINT "FK_hotel_amenity" FOREIGN KEY ("amenity_id") REFERENCES "amenities"("id")
+            PRIMARY KEY ("hotel_id", "amenity_id"),
+            FOREIGN KEY ("hotel_id") REFERENCES "hotels"("id") ON DELETE CASCADE,
+            FOREIGN KEY ("amenity_id") REFERENCES "amenities"("id") ON DELETE CASCADE
         )
+    `);
+
+    await queryRunner.query(`
+        CREATE TABLE "user_favorite_hotels" (
+            "user_id" uuid,
+            "hotel_id" integer,
+            PRIMARY KEY ("user_id", "hotel_id"),
+            FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE,
+            FOREIGN KEY ("hotel_id") REFERENCES "hotels"("id") ON DELETE CASCADE
+        )
+    `);
+
+    // Add foreign key for region_id in hotels table
+    await queryRunner.query(`
+        ALTER TABLE "hotels" ADD CONSTRAINT "fk_region" FOREIGN KEY ("region_id") REFERENCES "regions"("id")
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "hotel_amenities"`);
     await queryRunner.query(`DROP TABLE "user_favorite_hotels"`);
+    await queryRunner.query(`DROP TABLE "hotel_amenities"`);
     await queryRunner.query(`DROP TABLE "bookings"`);
-    await queryRunner.query(`DROP TABLE "hotels"`);
     await queryRunner.query(`DROP TABLE "amenities"`);
+    await queryRunner.query(`DROP TABLE "hotels"`);
     await queryRunner.query(`DROP TABLE "regions"`);
     await queryRunner.query(`DROP TABLE "users"`);
   }
